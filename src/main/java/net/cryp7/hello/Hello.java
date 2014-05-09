26b6cae7437c10100a7dca2a03d625e41c86f639
Stefan Schwoegler <stefan@cryp7.net>
2014-05-09 12:24:50 -0700

    added "banner"

--- src/main/java/net/cryp7/hello/Hello.java
+++ src/main/java/net/cryp7/hello/Hello.java
@@ -4,7 +4,10 @@ public class Hello
 {
 	public static void main (String[] args)
 	{
+		System.out.println("\n\n\n\n\n\n");
+		System.out.println("***********");
 		System.out.println("hello world");
+		System.out.println("***********");
 	}
 }
 
